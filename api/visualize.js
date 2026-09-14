1a93fef6c6c1e3e1d689eb474adb0618e716daac
av89930-tech <av89930@gmail.com>
2026-09-14 13:09:48 +0300

    Trigger Vercel deploy after Telegram token rotation

--- api/visualize.js
+++ api/visualize.js
@@ -1,3 +1,4 @@
+// Deployment trigger after Telegram token rotation; no runtime behavior change.
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
